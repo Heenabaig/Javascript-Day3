51064e0674b21d72b5379abbb9c7d6891a852208
Heenabaig <44297466+Heenabaig@users.noreply.github.com>
2020-07-23 22:00:11 +0530

    Add files via upload

---
+++ Third3.js
@@ -0,0 +1,14 @@
+
+var msg = 'enter marks of student:';
+
+var a = prompt(msg);
+if(a>80)
+    {
+     console.log('Grade - A');}
+else if(a<80 && a>40)
+    {
+     console.log('Grade - B');}
+      else
+        {console.log('Grade - C');}
+
+        
