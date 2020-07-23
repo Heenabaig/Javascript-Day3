03deb707e8da7fa61d665bc7f102b59508ce9969
Heenabaig <44297466+Heenabaig@users.noreply.github.com>
2020-07-23 22:23:36 +0530

    Update ThirdDswitch.js

--- ThirdDswitch.js
+++ ThirdDswitch.js
@@ -4,13 +4,13 @@ var a = 'enter marks of student:';
 var grade = prompt(a);
 
 switch(grade){
- case "80": // this is what will be typed (case sensitive)
-    result = "Grade A"; // assign some text to result - no brackets
+ case "80": 
+    result = "Grade A"; 
     break;
-case "50": // this is what will be typed (case sensitive)
+case "50": 
     result = "Grade B";
     break;
 default:
-    result = "please enter marks.." //something wasn't matched   
+    result = "please enter marks.."  
 }
 console.log(result);  
